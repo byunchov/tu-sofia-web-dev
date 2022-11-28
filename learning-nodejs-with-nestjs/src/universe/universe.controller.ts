0f25b9c1af0b2cdac64ef898205a9168769a328e
byunchov <bobi.iv.yu@gmail.com>
2022-11-28 19:09:30 +0200

    Initial comit.

--- learning-nodejs-with-nestjs/src/universe/universe.controller.ts
+++ learning-nodejs-with-nestjs/src/universe/universe.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UniverseDto } from './universe.dto';
 import { UniverseService } from './universe.service';
 
@@ -12,9 +12,27 @@ export class UniverseController {
     return universes;
   }
 
+  @Get(':id')
+  async getOneUniverses(@Param('id') id: number) {
+    const universe = await this.universeService.findOneUniverse(id);
+    return universe;
+  }
+
   @Post()
   async createOne(@Body() universe: UniverseDto) {
     const createdUniverse = await this.universeService.createOne(universe);
     return createdUniverse;
+  }
+
+  @Put()
+  async updateOne(@Body() universe: UniverseDto) {
+    const updatedUniverse = await this.universeService.updateOne(universe);
+    return updatedUniverse;
+  }
+
+  @Delete(':id')
+  async deleteOne(@Param('id') id: number) {
+    const deletedUniverse = await this.universeService.deleteOne(id);
+    return deletedUniverse;
   }
 }
